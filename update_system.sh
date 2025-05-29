a0aa8595e7759e2320c00a4558785900c34a056a
Ejimofor Nwoye <eknwoye@users.noreply.github.com>
2025-05-29 20:08:01 +0100

    Add files via upload

--- update_system.sh
+++ update_system.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+#
+# BASH Systems Update Script For Debian & Ubuntu Linux Derivatives On x86/64 Chip set Platform Architectures
+#
+# CODE REVISION & Author: Ejimofor Nwoye, Newspeak House, London, England @ 29/05/2025
 
+# Update and upgrade system packages
+sudo apt-get update && sudo apt-get dist-upgrade -y
+
+# Install build-essential package
+sudo apt-get install build-essential -y
+
+# Clean package cache
+sudo apt-get clean && sudo apt-get autoclean
